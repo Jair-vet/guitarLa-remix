fdd298c55bafb823c698ccc018c90cbafd7be4bf
Jair <jairaceves56@gmail.com>
2023-02-22 19:42:52 -0600

    Creamos el componente de Navegacion

---
+++ app/components/navegacion.jsx
@@ -0,0 +1,15 @@
+import { Link, useLocation } from '@remix-run/react'
+
+export const Navegacion = () => {
+
+    const location = useLocation()
+
+    return (
+        <nav className="navegacion">
+            <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Inicio</Link>
+            <Link to="/nosotros" className={location.pathname === '/nosotros' ? 'active' : ''}>Nosotros</Link>
+            <Link to="/tienda" className={location.pathname === '/tienda' ? 'active' : ''}>Tienda</Link>
+            <Link to="/block" className={location.pathname === '/block' ? 'active' : ''}>Block</Link>
+        </nav>
+    )
+}
