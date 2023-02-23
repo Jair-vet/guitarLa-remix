6626c00df3d0a6da49a86ac00db89ccc311d88d4
Jair <jairaceves56@gmail.com>
2023-02-22 19:43:16 -0600

    Agregamos el componente Navegacion

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,10 +1,9 @@
-import { Link, useLocation } from '@remix-run/react'
+import { Link } from '@remix-run/react'
 import logo from '../../public/img/logo.svg'
+import { Navegacion } from './navegacion'
 
 
 export const Header = () => {
-
-    const location = useLocation()
 
   return (
     <header className="header">
@@ -12,12 +11,8 @@ export const Header = () => {
             <Link to="/">
                 <img src={logo} alt="Imagen Logo" className='logo'/>
             </Link>
-            <nav className="navegacion">
-                <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Inicio</Link>
-                <Link to="/nosotros" className={location.pathname === '/nosotros' ? 'active' : ''}>Nosotros</Link>
-                <Link to="/tienda" className={location.pathname === '/tienda' ? 'active' : ''}>Tienda</Link>
-                <Link to="/block" className={location.pathname === '/block' ? 'active' : ''}>Block</Link>
-            </nav>
+
+           <Navegacion />
         </div>
     </header>
   )
