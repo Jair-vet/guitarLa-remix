6dc2dfca52ea9f17f3bd4b0e2e23a95146bd88e0
Jair <jairaceves56@gmail.com>
2023-02-27 15:46:04 -0600

    Primera forma de obtener la Data

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,4 +1,11 @@
 
+export async  function loader() {
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populata=imagen`)
+  const resultado = await respuesta.json()
+  
+  return{}
+}
+
 function Tienda() {
     return (
       <div>Tienda</div>
