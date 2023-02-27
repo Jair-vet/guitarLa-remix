4abe08f657921e397cd069faaf91a0c1eff6c5da
Jair <jairaceves56@gmail.com>
2023-02-27 16:27:46 -0600

    Mostramos los datos para cargalor en el servidor

--- app/models/guitarras.server.js
+++ app/models/guitarras.server.js
@@ -1,5 +1,5 @@
 export async function getGuitarras() {
-    const respuesta = await fetch(`${process.env.API_URL}/guitarras?populata=imagen`)
+    const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
     const resultado = await respuesta.json()
     return resultado
 }
