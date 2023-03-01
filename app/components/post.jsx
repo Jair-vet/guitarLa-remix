e79b1580c6e88cbfedb919a8415b87c845e752dc
Jair <jairaceves56@gmail.com>
2023-02-28 21:26:41 -0600

    Agregamos en el Post la fecha formateada

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,5 +1,5 @@
 import { Link } from "@remix-run/react"
-
+import { formatearFecha } from '~/utils/helpers';
 
 export const Post = ({post}) => {
 
@@ -10,7 +10,7 @@ export const Post = ({post}) => {
             <img className="imagen" src={imagen.data.attributes.formats.small.url} alt={`Imagen Block ${titulo}`} />
             <div className="contenido">
                 <h3>{titulo}</h3>
-                <p className="fecha">{publishedAt}</p>
+                <p className="fecha">{formatearFecha(publishedAt)}</p>
                 <p className="resumen">{contenido}</p>
                 <Link className="enlace" to={`/post/${url}`}>Leer Post</Link>
             </div>
