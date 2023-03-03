999665fa3eaa09a6ecf63aab7d4c67d1b3e6c854
Jair <jairaceves56@gmail.com>
2023-03-02 20:18:13 -0600

    Creamos las entradas de Tienda y Block

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,61 @@
+import { useLoaderData } from '@remix-run/react';
+import { getGuitarras } from '~/models/guitarras.server';
+import { getPosts } from '~/models/posts.server';
+import { ListadoGuitarras } from '~/components/listado-guitarras.jsx';
+import { ListadoPosts } from '../components/listado-posts';
+import stylesGuitarras from '~/styles/guitarras.css';
+import stylesPosts from '~/styles/block.css';
+
+
+export function meta(){
+
+}
+
+export function links(){
+    return [
+        {
+            rel: 'stylesheet',
+            href: stylesGuitarras
+        },
+        {
+            rel: 'stylesheet',
+            href: stylesPosts
+        },
+    ]
+}
+
+export async function loader(){
+
+    // Correr todos al mismo tiempo y tener un mejor performance
+    const [guitarras, posts] = await Promise.all([
+        getGuitarras(),
+        getPosts(),
+    ])
+
+    return {
+        guitarras: guitarras.data,
+        posts: posts.data
+    }
+}
 
 function Index() {
+
+    const { guitarras, posts } = useLoaderData()
+    
     return (
-        <div>Index</div>
+        <>
+            <main className="contenedor">
+                <ListadoGuitarras 
+                    guitarras={guitarras}
+                />
+            </main>
+
+            <section className='contenedor'>
+                <ListadoPosts 
+                    posts={posts}
+                />
+            </section>
+        </>
     )
 }
 
